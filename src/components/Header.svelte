d34f2b24e1d8d6dbc6f1d08c79ed292cbbb38a4f
Christian Mack <20512418+christian-mack@users.noreply.github.com>
2023-10-08 17:41:35 -0400

    version 1 draft complete

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -23,13 +23,13 @@
         <p>{tab.name}</p>
       </a>
     {/each}
-  </div>
 
-  <a
-    href="https://www.linkedin.com/in/christianmack2023"
-    target="_blank"
-    class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
-  >
-    <h4 class="relative z-9">Get in touch</h4>
-  </a>
+    <a
+      href="https://www.linkedin.com/in/christianmack2023"
+      target="_blank"
+      class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+    >
+      <h4 class="relative z-9">Get in touch</h4>
+    </a>
+  </div>
 </header>
---
+++ src/components/Main.svelte
@@ -0,0 +1,152 @@
+<script>
+  import Step from "./Step.svelte";
+
+  let steps = [
+    { name: "Portfolio", icon: "fa-solid fa-cart-shopping" },
+    { name: "Ultimate Todos", icon: "fa-solid fa-list-check" },
+    { name: "Pokédex", icon: "fa-solid fa-diagram-project" },
+  ];
+
+  let benefits = [
+    {
+      name: "a self-taught developer",
+      description:
+        "I am a full stack developer with a passion for building beautiful, functional, and responsive web applications.",
+      icon: "fa-solid fa-laptop-code",
+    },
+    {
+      name: "a problem solver",
+      description:
+        "I love solving problems and learning new things. I am always looking for ways to improve my skills and knowledge.",
+      icon: "fa-solid fa-lightbulb-on",
+    },
+    {
+      name: "a team player",
+      description:
+        "I am a team player who is always willing to help others and work together to achieve a common goal.",
+      icon: "fa-solid fa-users",
+    },
+    {
+      name: "a lifelong learner",
+      description:
+        "I am a lifelong learner who is always looking for ways to improve my skills and knowledge.",
+      icon: "fa-solid fa-book",
+    },
+  ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+  <section
+    id="introPage"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+  >
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="poppins text-violet-400">Christian</span> Mack
+        <br />Full Stack <span class="poppins text-violet-400">Developer</span>
+      </h2>
+      <p class="text-base">
+        My <span class="poppins text-violet-400">favorite tech</span> includes Javascript
+        (Next.js or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL or
+        Supabase!
+      </p>
+      <a
+        href="#"
+        target="_blank"
+        class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+      >
+        <h4 class="relative z-9">Get in touch &rarr;</h4>
+      </a>
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img
+        src="https://picsum.photos/500"
+        alt="profile"
+        class="object-cover z-[2] max-h-[70vh]"
+      />
+    </div>
+  </section>
+
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-2 text-center">
+      <h6 class="text-lg sm:text-xl md:text-2xl">
+        A few of my creative endeavors.
+      </h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        Curious to <span class="poppins text-violet-400">see</span> my work?
+      </h3>
+    </div>
+    <a
+      href="#"
+      target="_blank"
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+    >
+      <i class="fa-regular fa-circle-play" />
+      <p>Watch the video</p>
+    </a>
+
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+      <Step step={steps[0]}>
+        <p>
+          The store is a merchandising store created with <strong
+            class="text-violet-400"
+            >Next.js Commerce.js Stripe & Node.js + Express.js!</strong
+          > Commerce.js is a product CMS and Stripe is used for all transaction handling.
+        </p>
+      </Step>
+      <Step step={steps[1]}>
+        <p>
+          The store is a merchandising store created with <strong
+            class="text-violet-400"
+            >Next.js Commerce.js Stripe & Node.js + Express.js!</strong
+          > Commerce.js is a product CMS and Stripe is used for all transaction handling.
+        </p></Step
+      >
+      <Step step={steps[2]}>
+        <p>
+          The store is a merchandising store created with <strong
+            class="text-violet-400"
+            >Next.js Commerce.js Stripe & Node.js + Express.js!</strong
+          > Commerce.js is a product CMS and Stripe is used for all transaction handling.
+        </p>
+      </Step>
+    </div>
+  </section>
+
+  <section
+    id="about"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A bit <span class="poppins text-violet-400">about</span> me.
+      </h3>
+    </div>
+
+    <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+      I am . . .
+    </p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+      {#each benefits as benefit, index}
+        <div class="flex gap-6 sm:gap-8">
+          <p
+            class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+          >
+            0{index + 1}
+          </p>
+          <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+              {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+          </div>
+        </div>
+      {/each}
+    </div>
+  </section>
+</main>
